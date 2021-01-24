2234472edd6983d4405a86c9712c0f6b725b2e71
Park Jin Woo <pjw1480@naver.com>
2021-01-24 23:42:40 +0900

    좀비데이터 테이블 활용 다양화

--- Source/GP_Escape_From_Lab/Private/Zombie.cpp
+++ Source/GP_Escape_From_Lab/Private/Zombie.cpp
@@ -10,11 +10,9 @@
 #include "BehaviorTree/BehaviorTree.h"
 #include "Perception/PawnSensingComponent.h"
 
-//#include "GameFramework/Controller.h"
-//#include "GameFramework/Actor.h"
 
 
-USkeletalMesh* zombieMesh = nullptr;
+//USkeletalMesh* zombieMesh = nullptr;
 
 // Sets default values
 AZombie::AZombie()
@@ -22,10 +20,10 @@ AZombie::AZombie()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	if (!zombieMesh)
-		zombieMesh = ConstructorHelpers::FObjectFinder<USkeletalMesh>(TEXT("/Game/NonMovable/Zombie/SKMesh/Male/Ch10_nonPBR.Ch10_nonPBR")).Object;
+	//if (!zombieMesh)
+		//zombieMesh = ConstructorHelpers::FObjectFinder<USkeletalMesh>(TEXT("/Game/NonMovable/Zombie/SKMesh/Male/Ch10_nonPBR.Ch10_nonPBR")).Object;
 
-	GetMesh()->SetSkeletalMesh(zombieMesh);
+	//GetMesh()->SetSkeletalMesh(zombieMesh);
 	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FRotator(0.0f, -90.0f, 0.0f).Quaternion());
 	//this->OnTakeAnyDamage.AddDynamic(this, &AZombie::TakeDamage);
 	GetMesh()->SetCollisionProfileName("Ragdoll");
@@ -35,7 +33,7 @@ AZombie::AZombie()
 	// DT기반 제작.
 	//ZombieMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletalMeshComponent");
 	//RootComponent = ZombieMeshComp;
-	//DefaultZombieName = FName("");
+	DefaultZombieName = FName("");
 
 	//씬 초기화
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
@@ -54,10 +52,10 @@ void AZombie::BeginPlay()
 		PawnSensingComp->OnSeePawn.AddDynamic(this, &AZombie::OnPlayerCaught);
 	}
 	// DT기반 제작. 좀비 초기화.
-	//if (DefaultZombieName != "")
-	//{
-		//SetupZombie(DefaultZombieName);
-	//}
+	if (DefaultZombieName != "")
+	{
+		SetupZombie(DefaultZombieName);
+	}
 
 	
 }
@@ -107,15 +105,26 @@ void AZombie::SetupZombie(FName ZombieName)
 	// 밖에서 데이터 테이블을 불러왔는가?
 	if (ZombieDataTable)
 	{
+		
 		static const FString PString = FString("1");
 		// 불러온 테이블에서 행을 찾는다. ex) ZombieName == Police면 police줄을 가져온다.
 		ZombieData = ZombieDataTable->FindRow<FZombieData>(ZombieName, PString, true);
 		// 뭐 SetUp을 하면된다. 
+		
 		if (ZombieData)
 		{
-		
-			// ZombieData->ZombieMesh ==> 좀비 메쉬
-			// ZombieData->ZombieHP ==> 좀비HP
+			// 좀비 애님 클래스
+			GetMesh()->SetAnimInstanceClass(ZombieData->ZombieAnimBP->GeneratedClass);
+			// 좀비 sk메쉬
+			GetMesh()->SetSkeletalMesh(ZombieData->ZombieMesh);
+
+			//hp
+			hp = ZombieData->ZombieHP;
+			//speed
+			speed = ZombieData->ZombieSpeed;
+			//공격파워
+			attackPower = ZombieData->ZombieAttackPower;
+
 
 		}
 	}
@@ -128,7 +137,7 @@ void AZombie::OnPlayerCaught(APawn* pawn)
 
 	if (tempController)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("you have been"));
+		//
 		tempController->SetPlayerCaught(pawn);
 	}
 }
