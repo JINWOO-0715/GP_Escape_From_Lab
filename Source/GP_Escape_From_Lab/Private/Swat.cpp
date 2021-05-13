1626b86c4bea37679831a07e40fef0c84a5a147c
BabuBababu <poryou77@gmail.com>
2021-05-13 14:48:09 +0900

    대시 키 삭제

--- Source/GP_Escape_From_Lab/Private/Swat.cpp
+++ Source/GP_Escape_From_Lab/Private/Swat.cpp
@@ -1525,8 +1525,8 @@ void ASwat::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	PlayerInputComponent->BindAction("checkS", IE_Released, this, &ASwat::CheckReleasedWS);
 	PlayerInputComponent->BindAction("checkA", IE_Released, this, &ASwat::CheckReleasedAD);
 	PlayerInputComponent->BindAction("checkD", IE_Released, this, &ASwat::CheckReleasedAD);
-	PlayerInputComponent->BindAction("Dash", IE_Pressed, this, &ASwat::DashOn);
-	PlayerInputComponent->BindAction("Dash", IE_Released, this, &ASwat::DashOff);
+	//PlayerInputComponent->BindAction("Dash", IE_Pressed, this, &ASwat::DashOn);
+	//PlayerInputComponent->BindAction("Dash", IE_Released, this, &ASwat::DashOff);
 	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &ASwat::GunFireOn);
 	PlayerInputComponent->BindAction("Fire", IE_Released, this, &ASwat::GunFireOff);
 	PlayerInputComponent->BindAction("Knife", IE_Pressed, this, &ASwat::StabKnife);
